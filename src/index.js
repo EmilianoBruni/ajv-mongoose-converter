1219ac8aac37d817b0ec3ee26d1148e7bd00fe95
Emiliano Bruni <info@ebruni.it>
2024-02-01 15:57:30 +0100

    lint:fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-'use strict'
+'use strict';
 
-const _typeStringConvert = (ajvSchemaItem) => {
-    if ('format' in ajvSchemaItem &&
-        (ajvSchemaItem.format.match(/date/i))) return 'Date';
-    if ('pattern' in ajvSchemaItem &&
-        (ajvSchemaItem.pattern.match(/^\^\[0\-9(a\-f){1,2}\]\{24\}\$$/i)))
+const _typeStringConvert = ajvSchemaItem => {
+    if ('format' in ajvSchemaItem && ajvSchemaItem.format.match(/date/i))
+        return 'Date';
+    if (
+        'pattern' in ajvSchemaItem &&
+        ajvSchemaItem.pattern.match(/^\^\[0-9(a-f){1,2}\]\{24\}\$$/i)
+    )
         return 'ObjectId';
     return 'String';
-}
+};
 
-const _typeConvert = (ajvSchemaItem) => {
+const _typeConvert = ajvSchemaItem => {
     switch (ajvSchemaItem.type.toLowerCase()) {
         case 'string':
             return _typeStringConvert(ajvSchemaItem);
@@ -30,24 +32,26 @@ const _typeConvert = (ajvSchemaItem) => {
         case 'timestamp':
             return 'Date';
         default:
-            return 'Mixed'
+            return 'Mixed';
     }
-}
+};
 
 const convert = (ajvSchema, required = [], parent = null) => {
     let props;
     try {
-        props = ajvSchema.properties
-        if (!props) throw ''
+        props = ajvSchema.properties;
+        if (!props) throw '';
     } catch (error) {
-        throw new Error('Please initialize module with a valid schema = {properties: {...} }');
+        throw new Error(
+            'Please initialize module with a valid schema = {properties: {...} }'
+        );
     }
     const req = [...(ajvSchema.required || []), ...required];
 
-    const mooSchema = {}
+    const mooSchema = {};
 
     Object.keys(props).forEach(key => {
-        const prop = props[key]
+        const prop = props[key];
         // recursive if key has properties
         if ('properties' in prop) {
             mooSchema[key] = convert(prop, req, key);
@@ -58,13 +62,13 @@ const convert = (ajvSchema, required = [], parent = null) => {
         if (keyObj.type === 'Array' && prop.items && prop.items.type) {
             keyObj.type = Array(_typeConvert(prop.items));
         }
-        if (req.includes((parent ? `${parent}.` : '') + key)) keyObj.required = true;
-
+        if (req.includes((parent ? `${parent}.` : '') + key))
+            keyObj.required = true;
 
         mooSchema[key] = keyObj;
     });
 
     return mooSchema;
-}
+};
 
 module.exports = convert;
